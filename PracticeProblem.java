aae88f116cc5680b29e66158f57d12e2026eb582
Om Chokse <omchokse30@gmail.com>
2025-01-18 15:13:49 +0530

    code added

---
+++ PracticeProblem.java
@@ -0,0 +1,14 @@
+public class PracticeProblem{
+public static void main(String [] args){
+for(int i=1;i<=10;i++){
+if(i%2==0){
+System.out.print(i+" ");
+}
+}
+System.out.println();
+for(int i=1;i<=10;i++){
+if(i%2!=0){
+System.out.print(i+" ");
+}}
+}
+}
